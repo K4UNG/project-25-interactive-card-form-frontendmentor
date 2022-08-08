d275ac7b2bd65a52216fc6e82293cacd157af3b4
K4UNG <kgzinhein.my@gmail.com>
2022-08-09 00:00:02 +0630

    finish error handling

--- script.js
+++ script.js
@@ -7,10 +7,47 @@ const month = document.getElementById('card__month')
 const year = document.getElementById('card__year')
 const cvc = document.getElementById('card__cvc')
 
+const nameErr = document.getElementById('name__err')
+const numberErr = document.getElementById('number__err')
+const dateErr = document.getElementById('date__err')
+const cvcErr = document.getElementById('cvc__err')
+
 let errors = [1, 1, 1, 1, 1]
 
 form.onsubmit = e => {
     e.preventDefault()
+    if (errors[0] !== 0) {
+        inputs[0].classList.add('error')
+        nameErr.textContent = "Can't be blank"
+    }
+    if (errors[1] !== 0) {
+        inputs[1].classList.add('error')
+        const err = errors[1]
+        if (err === 1) {
+            numberErr.textContent = "Can't be blank"
+        } else if (err === 2) {
+            numberErr.textContent = "Wrong format, numbers only"
+        } else {
+            numberErr.textContent = 'Invalid number'
+        }
+    }
+    if (errors[2] !== 0 || errors[3] !== 0) {
+        const err1 = errors[2]
+        const err2 = errors[3]
+        if (err1 !== 0) inputs[2].classList.add('error')
+        if (err2 !== 0) inputs[3].classList.add('error')
+        if (err1 === 1 || err2 === 1) {
+            dateErr.textContent = "Can't be blank"
+        } else if (err1 === 2 || err2 === 2) {
+            dateErr.textContent = "Invalid"
+        }
+    }
+    if (errors[4] !== 0) {
+        inputs[4].classList.add('error')
+        const err = errors[4]
+        if (err === 1) cvcErr.textContent = "Can't be blank"
+        else cvcErr.textContent = 'Invalid'
+    }
 }
 
 inputs.forEach(input => {
@@ -19,8 +56,10 @@ inputs.forEach(input => {
         if ((key === 'e' || key === '-' || key === '+' || key === '_' || key === '=') && input.name !== 'name') e.preventDefault()
     }
     input.addEventListener('keyup', () => {
+        if (input.classList.contains('error')) input.classList.remove('error')
         const type = input.name
         if (type === 'name') {
+            nameErr.textContent = ''
             if (input.value.trim()) {
                 name.textContent = input.value.trim().toUpperCase()
                 errors[0] = 0
@@ -29,6 +68,7 @@ inputs.forEach(input => {
                 errors[0] = 1
             }
         } else if (type === 'number') {
+            numberErr.textContent = ''
             const value = input.value.trim().replace(/\s/g, '')
             const x = Number(value)
             if (x === 0) {
@@ -47,6 +87,7 @@ inputs.forEach(input => {
             let num = '0'.repeat(16-value.length) + value
             number.textContent = `${num.slice(0, 4)} ${num.slice(4, 8)} ${num.slice(8, 12)} ${num.slice(12, 16)}`
         } else if (type === 'cvc') {
+            cvcErr.textContent = ''
             const x = Number(input.value.trim());
             if (x === 0) {
                 errors[4] = 1
@@ -67,6 +108,7 @@ inputs.forEach(input => {
             }
             cvc.textContent = '0'.repeat(3-length) + value
         } else if (type === 'month' || type === 'year') {
+            dateErr.textContent = ''
             const value = input.value.trim()
             if (value.length > 2) {
                 input.value = value.slice(0,2)
